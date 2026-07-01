513cd866f08b2b2215aae933f590ada6761726f1
Avneesh Sharma <161005348+Avneesh-Sharma11@users.noreply.github.com>
2026-07-01 22:51:40 +0530

    Time: 5 ms (20.28%), Space: 8.4 MB (83.69%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        int i = 0;
+
+        while (x != 0) {
+            int r = x % 10;
+            if (rev < INT_MIN / 10 || rev > INT_MAX / 10) {
+                return 0;
+            }
+            rev = rev * 10 + r;
+            x /= 10;
+        }
+        return rev;
+    }
+};
